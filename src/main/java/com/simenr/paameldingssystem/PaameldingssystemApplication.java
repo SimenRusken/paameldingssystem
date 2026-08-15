7a11e2e1fe0f4cae669d718135647b8358e743e2
SimenRusken <simeneikevik.r@gmail.com>
2026-08-15 11:51:28 +0200

    Opprettet spring boot prosjektet

---
+++ src/main/java/com/simenr/paameldingssystem/PaameldingssystemApplication.java
@@ -0,0 +1,13 @@
+package com.simenr.paameldingssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaameldingssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaameldingssystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simenr/paameldingssystem/PaameldingssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simenr.paameldingssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaameldingssystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
